ec4475779863f8f6c524e53f3a8d3327676db30d
K L Bhanu <klbm9999@gmail.com>
2017-04-12 17:04:47 +0530

    corrected minor spelling mistake in app_templates

--- app_templates/main_template.php
+++ app_templates/main_template.php
@@ -1,31 +1,31 @@
-<?php require 'app_template/before_app_name.php'; ?>
+<?php require 'app_templates/before_app_name.php'; ?>
 		<!-- @yield('app_name') -->
-<?php require 'app_template/before_app_maker.php'; ?>
+<?php require 'app_templates/before_app_maker.php'; ?>
 		<!-- @yield('app_maker') -->
-<?php require 'app_template/before_app_image.php'; ?>
+<?php require 'app_templates/before_app_image.php'; ?>
 		<!-- @yield('app_image') -->
-<?php require 'app_template/before_app_description.php'; ?>
+<?php require 'app_templates/before_app_description.php'; ?>
 		<!-- @yield('app_description') -->
-<?php require 'app_template/before_app_link.php'; ?>
+<?php require 'app_templates/before_app_link.php'; ?>
 		<!-- @yield('app_link') -->
-<?php require 'app_template/before_link2.php'; ?>
+<?php require 'app_templates/before_link2.php'; ?>
 		<!-- @yield('link2') -->
-<?php require 'app_template/before_img1.php'; ?>
+<?php require 'app_templates/before_img1.php'; ?>
 		<!-- @yield('img1') -->
-<?php require 'app_template/before_text1.php'; ?>
+<?php require 'app_templates/before_text1.php'; ?>
 		<!-- @yield('text1') -->
-<?php require 'app_template/before_text2.php'; ?>
+<?php require 'app_templates/before_text2.php'; ?>
 		<!-- @yield('text2') -->
-<?php require 'app_template/before_img2.php'; ?>
+<?php require 'app_templates/before_img2.php'; ?>
 		<!-- @yield('img2') -->
-<?php require 'app_template/before_img3.php'; ?>
+<?php require 'app_templates/before_img3.php'; ?>
 		<!-- @yield('img3') -->
-<?php require 'app_template/before_text3.php'; ?>
+<?php require 'app_templates/before_text3.php'; ?>
 		<!-- @yield('text3') -->
-<?php require 'app_template/before_text4.php'; ?>
+<?php require 'app_templates/before_text4.php'; ?>
 		<!-- @yield('text4') -->
-<?php require 'app_template/before_img4.php'; ?>
+<?php require 'app_templates/before_img4.php'; ?>
 		<!-- @yield('img4') -->
-<?php require 'app_template/before_extra.php'; ?>
+<?php require 'app_templates/before_extra.php'; ?>
 		<!-- @yield('extra') -->
 <?php require 'footer.php'; ?>
--- apps-backup-and-reinstall.php
+++ apps-backup-and-reinstall.php
@@ -1,27 +1,27 @@
-<?php require 'app_template/before_app_name.php'; ?>
+<?php require 'app_templates/before_app_name.php'; ?>
 		Apps Backup and Reinstall
-<?php require 'app_template/before_app_maker.php'; ?>
+<?php require 'app_templates/before_app_maker.php'; ?>
 		By Sciencenext
-<?php require 'app_template/before_app_image.php'; ?>
+<?php require 'app_templates/before_app_image.php'; ?>
 		apps backup and reinstall.png
-<?php require 'app_template/before_app_description.php'; ?>
+<?php require 'app_templates/before_app_description.php'; ?>
 		With beautiful glossy design with colorful themes have 500k download worldwide and still counting. One of the most successful player on play store
-<?php require 'app_template/before_app_link.php'; ?>
+<?php require 'app_templates/before_app_link.php'; ?>
 		https://play.google.com/store/apps/details?id=com.touchfield.appbackuprestore "></a>
-<?php require 'app_template/before_img1.php'; ?>
+<?php require 'app_templates/before_img1.php'; ?>
 		images/abar1.png
-<?php require 'app_template/before_text1.php'; ?>
+<?php require 'app_templates/before_text1.php'; ?>
 		It’s categorized as Top free app in PRODUCTIVITY category on Google play world wide.
-<?php require 'app_template/before_text2.php'; ?>
+<?php require 'app_templates/before_text2.php'; ?>
 		Apps backup and restore app made with wow teal material design.
-<?php require 'app_template/before_img2.php'; ?>
+<?php require 'app_templates/before_img2.php'; ?>
 		images/abar2.png
-<?php require 'app_template/before_img3.php'; ?>
+<?php require 'app_templates/before_img3.php'; ?>
 		images/abar3.png
-<?php require 'app_template/before_text3.php'; ?>
+<?php require 'app_templates/before_text3.php'; ?>
 		It has more than 50k downloads worldwide and still counting.
-<?php require 'app_template/before_text4.php'; ?>
+<?php require 'app_templates/before_text4.php'; ?>
 		One of the most successful apps on Play store
-<?php require 'app_template/before_img4.php'; ?>
+<?php require 'app_templates/before_img4.php'; ?>
 		images/abar4.png
 <?php require 'footer.php'; ?>
